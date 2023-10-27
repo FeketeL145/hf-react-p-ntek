88c617cacf9d83f1ec2ee1c38b997443bbbd9475
FeketeL145 <feketel@kkszki.hu>
2023-10-27 09:15:57 +0200

    Feladat kész

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect} from 'react';
 
 function App() {
+  const [clickCount, setClickCount] = useState(0);
+
+  useEffect(() =>{
+    document.title = "Kattintások száma: " + clickCount;
+    console.log(clickCount);
+    
+  }, [clickCount]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+    Kattintások száma: {clickCount}
+    <br/>
+    <span>
+    <button
+    className='btn btn-primary'
+    onClick={() => {
+      setClickCount((elozo) => elozo + 1);
+    }}
+    >
+      Kattints!
+    </button>
+    </span>
+    <BoxComponent clickCount={clickCount}></BoxComponent>
+    
+    </center>
   );
 }
 
+function BoxComponent(props){
+  return(
+    <div
+    className="p-2 m-5 rounded"
+    style=
+      {{
+        width: "200px",
+        height: "200px",
+        backgroundColor: "green",
+      }}
+    >
+      {props.clickCount}
+    </div>
+  );
+}
 export default App;
